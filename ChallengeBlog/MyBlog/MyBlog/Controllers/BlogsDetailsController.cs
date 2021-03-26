b79b5349200a6cb0a2e547cd0d99c5dfbb919b19
namirgon <namirgon@gmail.com>
2021-03-26 12:44:30 -0300

    Primera iteración Migrations/20210324160349_InitialCreate.cs Index.cshtml Edit.cshtml Delete.cshtml Create.cshtml

---
+++ ChallengeBlog/MyBlog/MyBlog/Controllers/BlogsDetailsController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBlog.Models;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBlog.Controllers
+{
+    public class BlogsDetailsController : Controller
+    {
+
+        private DataBaseBlog _context;
+
+        public BlogsDetailsController(DataBaseBlog context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<BlogDetails> listBlogs = _context.BlogDetails.OrderByDescending(b => b.CreationDate);
+
+            return View(listBlogs);
+        }
+
+
+
+        public IActionResult Details(int Id)
+        {
+            var detalle = _context.BlogDetails.Find(Id);
+
+            return View(detalle);
+        }
+
+        public IActionResult Edit(int? Id)
+        {
+            if(Id == null || Id == 0)
+            {
+
+                return NotFound();
+            }
+
+            var blog = _context.BlogDetails.Find(Id);
+
+            if (blog == null)
+            {
+
+                return NotFound();
+            }
+          
+            return View(blog);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(BlogDetails unBlog, List<IFormFile> Picture)
+        {
+
+            foreach (var item in Picture)
+            {
+
+                if (item.Length > 0)
+                {
+                    using (var unStream = new MemoryStream())
+                    {
+                        await item.CopyToAsync(unStream);
+                        unBlog.Picture = unStream.ToArray();
+                    }
+                }
+            }
+            _context.BlogDetails.Update(unBlog);
+            _context.SaveChanges();
+            //return View();
+            return Redirect("~/BlogsDetails/Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BlogDetails unBlog, List<IFormFile> Picture) 
+        {
+
+            foreach (var item in Picture)
+            {
+
+                if (item.Length > 0)
+                {
+                    using (var unStream = new MemoryStream())
+                    {
+                        await item.CopyToAsync(unStream);
+                        unBlog.Picture = unStream.ToArray();
+                    }
+                }
+            }
+            _context.BlogDetails.Add(unBlog);
+            _context.SaveChanges();
+            //  return View();
+
+            return Redirect("~/BlogsDetails/Index");
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+
+                return NotFound();
+            }
+
+            var blog = _context.BlogDetails.Find(Id);
+
+            if (blog == null)
+            {
+
+                return NotFound();
+            }
+
+            return View(blog);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteBlog(int? Id)
+        {
+
+            var unBlog = _context.BlogDetails.Find(Id);
+
+            if (unBlog == null)
+            {
+
+                return NotFound();
+            }
+           
+            
+            _context.BlogDetails.Remove(unBlog);
+            _context.SaveChanges();
+
+            return Redirect("~/BlogsDetails/Index");
+        }
+
+
+    }
+}
---
+++ ChallengeBlog/MyBlog/MyBlog/Models/DataBaseBlog.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog.Models
+{
+    public class DataBaseBlog : DbContext
+    {
+
+        public DataBaseBlog(DbContextOptions<DataBaseBlog> options) : base(options)
+        {
+
+
+
+        }
+
+        public DbSet<BlogDetails> BlogDetails { get; set; }
+   
+    }
+
+    public class BlogDetails
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Titulo")]
+        public String Title { get; set; }
+
+        [Required]
+        [Display(Name = "Contenido")]
+        public String Contents { get; set; }
+
+
+
+        [Required]
+        [Display(Name = "Categoria")]
+        public String CategoryName { get; set; }
+
+        [Required]
+        [Display(Name = "Imagen")]
+        public byte[] Picture { get; set; }
+
+
+        [Required]
+        [Display(Name = "Fecha de Creacion")]
+        [DataType(DataType.Date)]
+        public DateTime CreationDate { get; set; }
+
+
+   
+
+
+    }
+}
+
+
